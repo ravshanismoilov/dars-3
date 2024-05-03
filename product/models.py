b142aa179145c230bd63dc7ac6cc4163983988fa
ravshanismoilov <r.ismoilov1001@mail.com>
2024-05-03 19:36:40 +0500

    prosta comit

---
+++ product/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=155)
+
+    class Meta:
+        db_table = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    category = models.ForeignKey(to="Category", on_delete=models.CASCADE)
+    model = models.CharField(max_length=150)
+    make = models.CharField(max_length=150)
+    body = models.TextField()
+    price = models.IntegerField()
+    image = models.ImageField(upload_to="products/", blank=True, null=True)
+
+    class Meta:
+        db_table = 'products'
+
+    def __str__(self):
+        return f"{self.category.name}, {self.model}"
